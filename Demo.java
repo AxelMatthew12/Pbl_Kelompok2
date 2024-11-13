9588acd571a59f38b6c600a4d133f924a628a729
fasyadita <144809443+fasyadita@users.noreply.github.com>
2024-11-13 10:15:29 +0700

    Update Demo.java
     2.5 Polymorphic Arguments & instanceOf fasya

--- Demo.java
+++ Demo.java
@@ -1,17 +1,19 @@
 public class Demo {
     public static void main(String[] args) {
         Dosen dosen1 = new Dosen("19940201", "Widia, S.Kom, M.Kom", "199402");
+        TenagaKependidikan tendik1 = new TenagaKependidikan("19750301", " Aida, A.Md.", "Tenaga Administrasi");
 
-        Pegawai pegawai1 = dosen1;
+        train(dosen1);
+        train(tendik1);
+    }
 
-        System.out.println(pegawai1.nip);
-        System.out.println(pegawai1.nama);
-        pegawai1.displayInfo();
+    public static void train(Pegawai pegawai) {
+        pegawai.displayInfo();
+        System.out.println("Mengenalkan lingkungan kampus");
+        System.out.println("Menginfokan SOP.Juknis");
 
-        Dosen newDosen = (Dosen) pegawai1;
-
-        System.out.println(newDosen.nama);
-        System.out.println(newDosen.nidn);
-        newDosen.mengajar();
+        if (pegawai instanceof Dosen) {
+            System.out.println("Memberikan pelatihan pedagogik");
+        }
     }
 }
